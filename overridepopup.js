40ce850be5046cc05348e7c0d1dd7e10830a26c5
John French <johnfren@indiana.edu>
2016-01-25 22:02:57 -0500

    fixed script injection

--- overridepopup.js
+++ overridepopup.js
@@ -1,6 +1,32 @@
-$(document).change(removeDialog);
-
 function removeDialog(){
 	var dialog = $('play-limit-dialog').parent();
+	if (dialog){
+		alert("dialog detected");
+	}
 	$(dialog).remove();
 }
+
+if ($(document)){
+	$(document).on('change', removeDialog());
+}
+/*
+chrome.runtime.onInstalled.addListener(function() {
+	alert("listener attached");
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		chrome.declarativeContent.onPageChanged.addRules([
+			{
+				conditions: [
+					new chrome.declarativeContent.PageStateMatcher({
+						pageUrl: { hostEquals: 'www.bandcamp.com' },
+					})
+				],
+				actions: [ new chrome.declarativeContent.ShowPageAction(), 
+					function(){
+						alert("on bandcamp page");
+					}, removeDialog()
+				]
+			}
+		]);
+	});
+});
+*/
